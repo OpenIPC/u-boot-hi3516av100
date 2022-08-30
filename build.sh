cb9237487ae1a6afff2f00edee0ae4eb9fdd4a3c
Dmitry Ermakov <de@dimer.org.ua>
2022-08-30 23:04:20 +0300

    Fix build script

--- build.sh
+++ build.sh
@@ -4,10 +4,11 @@ export ARCH=arm
 export CROSS_COMPILE=${CROSS_COMPILE:-arm-hisiv510-linux-}
 
 OUTPUTDIR="${HOME}/src/hisi/uboot"
-SOCS="hi3516d hi3516a"
+SOCS="hi3516a hi3516d"
 
 for soc in ${SOCS};do
 
+sed -i s/hi3516.v100/${soc}v100/ include/configs/hi3516a.h
 make clean
 make hi3516a_config
 cp reg_info_${soc}.bin .reg
